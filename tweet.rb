4e08852f2d923447ebb294e50304612b2c61973b
sasamijp <k.seiya28@gmail.com>
2014-03-06 00:12:20 +0900

    歯医者で治療中に寝た
    tweet.rbを叩くとマルコフ連鎖でツイートできるようにした

---
+++ tweet.rb
@@ -0,0 +1,86 @@
+# -*- encoding: utf-8 -*-
+require 'natto'
+require 'tweetstream' #これがないとなぜか動かない
+require 'twitter'
+require './key.rb'
+
+
+Twitter.configure do |config|
+   config.consumer_key        = Const::CONSUMER_KEY
+   config.consumer_secret     = Const::CONSUMER_SECRET
+   config.oauth_token            = Const::ACCESS_TOKEN
+   config.oauth_token_secret  = Const::ACCESS_TOKEN_SECRET
+end
+TweetStream.configure do |config|
+   config.consumer_key        = Const::CONSUMER_KEY
+   config.consumer_secret     = Const::CONSUMER_SECRET
+   config.oauth_token            = Const::ACCESS_TOKEN
+   config.oauth_token_secret  = Const::ACCESS_TOKEN_SECRET
+   config.auth_method            = :oauth
+end
+
+def readdata
+  s = File.read("data.txt", :encoding => Encoding::UTF_8)
+  s = s.split("\n")
+  stat = []
+  s.each do |s|
+    stat.push(s.split("||")[0])
+  end
+  return stat
+end
+
+
+class Malkov
+
+  def initialize(*str)
+    @array = str
+    @array.flatten!
+    @nm = Natto::MeCab.new
+  
+    statements = []
+    @array.each do |segments|
+
+      statement = segments
+
+      @nm.parse(segments) do |n|
+        type = n.feature.split(",")[0]
+        if type == "名詞"
+          statements.push("#{statement.split(n.surface)[0]} #{n.surface}")
+          statements.push("#{n.surface} #{statement.split(n.surface)[1]}")
+        end
+      end
+
+    end
+
+    @dictionary = statements
+  end
+
+  def create
+
+    text = []
+    first = @dictionary.sample
+    text.push(first)
+    nextwords = @dictionary.select do |item|
+      (item.split[0] == first.split[1])
+    end
+
+    3.times do
+      unless nextwords == nil
+      	nextwords.delete_if do |word|
+          text.to_s.include?(word)
+      	end
+        nextword = nextwords.compact.sample
+        unless nextword == nil
+          text.push(nextword)
+        end  
+      end
+    end
+
+    return text.join("").gsub(" ","")
+  end
+
+end
+
+m = Malkov.new(readdata)
+
+Twitter.update m.create
